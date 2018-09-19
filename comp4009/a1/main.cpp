5f041202ae324dcce45b4ffb764d40a8a54b7b34
mic-max <mike.ian.maxwell@gmail.com>
2018-09-19 00:46:50 -0400

    starting progress.. splitting image into quadrants

--- comp4009/a1/main.cpp
+++ comp4009/a1/main.cpp
@@ -1,40 +1,86 @@
-
-#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <math.h>
 #include <cilk/cilk.h>
-#include "hwtimer.h"
+
 using namespace std;
 
-int fib(int n) 
-	{
-		if (n < 2)
-			return n;
-		int a = cilk_spawn fib(n-1);
-		int b = cilk_spawn fib(n-2);
-		cilk_sync;
-		return a + b;
-	};
+int m, n, k;
+int **X, **Y;
 
+void median_filter(int minX, int minY, int maxX, int maxY) {
+	if (minX > maxX || minY > maxY) return; // Or stop these from spawning below ?
+	if (minX == maxX && minY == maxY) {
+		// cout << minX << "," << minY << " ---> " << maxX << ',' << maxY << endl;
+		// cout << flush;
+		// one pixel to compute the median of the surrounding values
+		int r = X[minX][minY];
+		// Y[minX][minY] = r;
+		return;
+	}
 
-int main(int argc, char* argv[])
-	{
-		if (argc != 2) {
-			cout << "Usage: fib <num>" << endl;
-			return 1;
+	int midX = floor((minX + maxX) / 2);
+	int midY = floor((minY + maxY) / 2);
+
+	// cout << minX << "," << minY << " ---> " << maxX << ',' << maxY << endl;
+	// cout << flush;
+	// spawn 4 threads, each working on an equal amount of the input 
+	cilk_spawn median_filter(minX    , minY    , midX, midY); // top-left
+	cilk_spawn median_filter(midX + 1, minY    , maxX, midY); // top-right
+	cilk_spawn median_filter(minX    , midY + 1, midX, maxY); // bottom-left
+	cilk_spawn median_filter(midX + 1, midY + 1, maxX, maxY); // bottom-right
+	cilk_sync;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		cout << "Usage: median-filter <input> <output>" << endl;
+		return 1;
+	}
+	
+	// read input file
+	ifstream input(argv[1]);
+	if(input.is_open()) {
+		input >> m >> n >> k;
+
+		// initialize arrays
+		X = new int*[n];
+		Y = new int*[n];
+		for (int i = 0; i < n; i++) {
+			X[i] = new int[m];
+			Y[i] = new int[m];
 		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				input >> X[i][j];
+
+			}
+		}
+		input.close();
+	} else {
+		cout << "Input file: '" << argv[1] << "' could not be opened.";
+		return 2;
+	}
+
+	median_filter(0, 0, m - 1, n - 1);
 	
-		hwtimer_t timer;
-		initTimer(&timer);
+	// write output file
+	ofstream output(argv[2]);
+	if(output.is_open()) {
+		for (int i = 0; i < n; i++) {
+			// If the last space is not allower, only do j < m-1, and then after the for loop print Y[i, m-1] to stream
+			// or is there a way to insert a delete char to the stream
+			for (int j = 0; j < m; j++) {
+				output << Y[i][j] << ' ';
+			}
+			output << endl;
+		}
+		output.close();
+	} else {
+		cout << "Output file: '" << argv[2] << "' could not be opened.";
+		return 3;
+	}
 	
-		int param = atoi(argv[1]);
-	
-		startTimer(&timer);
-		int answer = fib(param);
-		stopTimer(&timer);
-		int fibTime = getTimerNs(&timer);
-	
-		cout << "fib(" << param << ") = " << answer << endl;
-		cout << "Total time: " << fibTime << "ns" << endl;
-	
-		return 0;
-	};
+	return 0;
+}
